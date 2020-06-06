1219c7391a64fa2def53e8857ce410c3c75e700b
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-06-06 16:05:41 +0200

    Updates
    1. Updated Sjoeke's script (Saw some errors occuring in her script related to it being a monobehaviour rather than taking rules from BaseAI)
    2. Added Props but made sure they dont disturb the ships (have to discuss their usge during the meeting tomorrow).

--- Assets/Scripts/SjoekeAI.cs
+++ Assets/Scripts/SjoekeAI.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SjoekeAI : MonoBehaviour
+public class SjoekeAI : BaseAI
 {
-    // Start is called before the first frame update
-    void Start()
+    public override IEnumerator RunAI()
     {
-        
+        for (int i = 0; i < 10; i++)
+        {
+            yield return Ahead(50);
+            yield return RapidFire();
+            yield return FireFront(1);
+            yield return TurnLookoutLeft(45);
+            yield return TurnLeft(360);
+            yield return FireLeft(1);
+            yield return TurnLookoutRight(150);
+            yield return Back(300);
+            yield return FireRight(1);
+            yield return TurnLookoutLeft(90);
+            yield return TurnRight(90);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnScannedRobot(ScannedRobotEvent e)
     {
-        
+        //commented out the debug. - Aadi
+        //Debug.Log("Ship detected: " + e.Name + " at distance: " + e.Distance);
     }
 }
