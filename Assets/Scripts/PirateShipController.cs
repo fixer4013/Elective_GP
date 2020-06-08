061e1bc4b5628b9ecfdb6039d5aebf6be3301495
ZipNotRar <61015917+ZipNotRar@users.noreply.github.com>
2020-06-08 10:55:41 +0200

    Updates
    1. A ghost came and typed all these things (I didnt code any of this)
    2. The ghost fixed all the HP related problems.
    3. Ships are given ammuniion and weight.

--- Assets/Scripts/PirateShipController.cs
+++ Assets/Scripts/PirateShipController.cs
@@ -30,6 +30,7 @@ public class PirateShipController : MonoBehaviour
     private float SeaSize = 500.0f;
     private float RotationSpeed = 180.0f;
     public float currentBoatSpeed;
+    //Added Inaccuracy. -Sjoeke
     float inaccuracy = 15;
     //float randomAngle = Random.Range(-15, 15);
     
@@ -37,14 +38,14 @@ public class PirateShipController : MonoBehaviour
 
     //all values for the different types of ammo. -Martin, Maxym
     private int maxAmmoCap = 5;
-    private int ammunition;
+    private int weight;
     public int cannonballs;
     public int mines;
 
     //maxHP & currentHP to create the skeleton of health of ship obeying Ilja's AI. - Aadi.
-    public int maxHP = 100;
-    public int currentHP;
-    public int LowHP = 20;
+    private int maxHP;
+    private int currentHP;
+    public int LowHP;
 
     //Speed boost variables -Maxym
     public bool speedBoostCooldown;
@@ -54,13 +55,17 @@ public class PirateShipController : MonoBehaviour
     void Awake()
     {
         //currentHp is same as maxHP at the game's start. - Aadi.
+        maxHP = 100;
+        LowHP = 20;
         currentHP = maxHP;
+        cannonballs = 2;
+        weight = 2;
     }
 
     private void Update()
     {
         //calculate boat speed based on the amount of ammo the ship carries. -Martin, Maxym
-        currentBoatSpeed = (BoatSpeed - 14 * ammunition) * speedBoostValue;
+        currentBoatSpeed = (BoatSpeed - 14 * weight) * speedBoostValue;
 
     }
 
@@ -134,35 +139,37 @@ public class PirateShipController : MonoBehaviour
     }
 
     //Added so you cant shoot when you dont have any cannonballs. -Maxym
-    //
+    //Added Inaccuracy. -Sjoeke
     public IEnumerator __FireFront(float power) {
         if (cannonballs > 0)
         {
             GameObject newInstance = Instantiate(CannonBallPrefab, CannonFrontSpawnPoint.position, Quaternion.Euler(0, Random.Range(-inaccuracy, inaccuracy), 0) * CannonFrontSpawnPoint.rotation);
             cannonballs -= 1;
-            ammunition -= 1;
+            weight -= 1;
         }
         yield return new WaitForFixedUpdate();
     }
 
     //Added so you cant shoot when you dont have any cannonballs. -Maxym
+    //Added Inaccuracy. -Sjoeke
     public IEnumerator __FireLeft(float power) {
         if (cannonballs > 0)
         {      
             GameObject newInstance = Instantiate(CannonBallPrefab, CannonLeftSpawnPoint.position, Quaternion.Euler(0, Random.Range(-inaccuracy, inaccuracy), 0) * CannonLeftSpawnPoint.rotation);
             cannonballs -= 1;
-            ammunition -= 1;
+            weight -= 1;
         }
         yield return new WaitForFixedUpdate();
     }
 
     //Added so you cant shoot when you dont have any cannonballs. -Maxym
+    //Added Inaccuracy. -Sjoeke
     public IEnumerator __FireRight(float power) {
         if (cannonballs > 0)
         {
             GameObject newInstance = Instantiate(CannonBallPrefab, CannonRightSpawnPoint.position, Quaternion.Euler(0, Random.Range(-inaccuracy, inaccuracy), 0) * CannonRightSpawnPoint.rotation);
             cannonballs -= 1;
-            ammunition -= 1;
+            weight -= 1;
         }
         yield return new WaitForFixedUpdate();
     }
@@ -173,7 +180,7 @@ public class PirateShipController : MonoBehaviour
         {
             GameObject newInstance = Instantiate(MinePrefab, transform.position, transform.rotation);
             mines -= 1;
-            ammunition -= 3;
+            weight -= 3;
         }
         yield return new WaitForFixedUpdate();
     }
@@ -232,17 +239,17 @@ public class PirateShipController : MonoBehaviour
         {
             if (other.GetComponent<Ammunition>().ammoType == "Cannonball")
             {
-                if (ammunition < maxAmmoCap)
+                if (weight < maxAmmoCap)
                 {
-                    ammunition += 1;
+                    weight += 1;
                     cannonballs += 1;
                 }
             }
             if (other.GetComponent<Ammunition>().ammoType == "Mine")
             {
-                if (ammunition < maxAmmoCap - 2)
+                if (weight < maxAmmoCap - 2)
                 {
-                    ammunition += 3;
+                    weight += 3;
                     mines += 1;
                 }
             }
@@ -255,6 +262,10 @@ public class PirateShipController : MonoBehaviour
     public void TakeDamage(int damage)
     {
         currentHP -= damage;
+        if (currentHP <= 0)
+        {
+            Destroy(this.gameObject);
+        }
     }
     //Perk1 - Aadi.
     public void PerkOne()
@@ -298,6 +309,7 @@ public class PirateShipController : MonoBehaviour
     }
 
     //Added a function for rapid fire - Martin
+    //Added Inaccuracy. -Sjoeke
     public IEnumerator __RapidFire()
     {
         if (cannonballs > 0)
@@ -306,7 +318,7 @@ public class PirateShipController : MonoBehaviour
             for (int i = 0; i < currentCannonBalls; i++)
             {
                 GameObject newInstance = Instantiate(CannonBallPrefab, CannonFrontSpawnPoint.position, Quaternion.Euler(0, Random.Range(-inaccuracy, inaccuracy), 0) * CannonFrontSpawnPoint.rotation);
-                ammunition--;
+                weight--;
                 cannonballs--;
                 yield return new WaitForSeconds(0.5f);
             }
