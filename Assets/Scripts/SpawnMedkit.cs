82c94ebc6de92626daaebc7fe6eb29d988a04ad3
Fixer4013 <fixer4013@gmail.com>
2020-06-09 23:55:06 +0200

    Added a spawner for the medkit
    The spawner is a those small lighthouses that indicate the ships for potential danger (placeholder for now)

---
+++ Assets/Scripts/SpawnMedkit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnMedkit : MonoBehaviour
+{
+    public GameObject MedKitPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnMedKit());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator SpawnMedKit()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(60);
+            Instantiate(MedKitPrefab, new Vector3(Random.Range(-400, 400), 0, Random.Range(-400, 400)), Quaternion.identity);
+
+        }
+    }
+}
