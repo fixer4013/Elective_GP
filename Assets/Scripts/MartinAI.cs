04dff96fb4bcc7594ce3605c425dcb09a86b1d17
Fixer4013 <fixer4013@gmail.com>
2020-06-12 17:12:25 +0200

    Update to Martin AI v2

--- Assets/Scripts/MartinAI.cs
+++ Assets/Scripts/MartinAI.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Advertisements;
 
 public class MartinAI : BaseAI
 {
     private string mode = "patrol";
-     
+
+    private Vector3 enemyPosition;
+
+    private float enemyRotation; 
+    
    
     // Start is called before the first frame update
     public override IEnumerator RunAI()
@@ -19,25 +24,52 @@ public class MartinAI : BaseAI
             switch (mode)
             { 
                 case "patrol":
-                    yield return TurnLookoutRight(180);
-                    yield return TurnLeft(Random.Range(30, 180));
-                    yield return Ahead(Random.Range(10, 500));
-                    yield return TurnLookoutLeft(180);
-                    yield return TurnRight(Random.Range(30, 180));
-                    
-                    
+                    if (mode != "hunt")
+                    {
+                        yield return TurnLeft(Random.Range(30, 180));
+                        yield return Ahead(Random.Range(10, 800));
+                        yield return TurnLookoutLeft(180);
+                        yield return TurnRight(Random.Range(30, 180));
+                        yield return Ahead(Random.Range(200, 300));
+                        yield return TurnRight(Random.Range(30, 90));
+                        yield return TurnLookoutLeft(Random.Range(30, 360));
+                    }
                     if (Ship.currentHP <= 40)
                     {
                         mode = "flee";
                     }
                     break;    
                     
+                
+                //I want to get the angle between the two vectors of the positions of my ship and an enemy ship
+                //Based on that information I want my ship to rotate sideways of the enemy ship and shoot a cannonball 
+                
                 case "hunt":
-                    yield return 0;
+                    
+                    yield return FireFront(1);
+                    yield return FireLeft(1);
+                    yield return FireRight(1);
+
+                    mode = "patrol";
+
+                    /*Vector3 targetDir = enemyPosition - Ship.currentPosition;
+                    float angle = Vector3.Angle(targetDir, Vector3.forward);
+                    
+                    Debug.Log(angle);
+                    if (angle < 8)
+                    {
+                        yield return FireFront(1);
+                    }
+
+                    if (angle > 8)
+                    {
+                        
+                    }
+                    */
+                    
                     break;
                 
                 case "flee":
-                    yield return RapidFire();
                     yield return TurnLeft(180);
                     yield return TurnLookoutLeft(180);
                     yield return Ahead(500);
@@ -56,14 +88,62 @@ public class MartinAI : BaseAI
     // Update is called once per frame
     public override void OnScannedRobot(ScannedRobotEvent e)
     {
-        Debug.Log("Ship detected: " + e.Name + " at distance: " + e.Distance);
-        if (e.Name == "boat")
+        if (e.Name == "Maxym")
         {
-            mode = "hunt";
+            e.Position = enemyPosition;
+            e.Rotation = enemyRotation;
+            if (Ship.currentHP > 40)
+            {
+                mode = "hunt";
+            }
+            else 
+            {
+                mode = "patrol";
+            }
         }
+        
+        if (e.Name == "Ruben")
+        {
+            e.Position = enemyPosition;
+            e.Rotation = enemyRotation;
+            if (Ship.currentHP > 40)
+            {
+                mode = "hunt";
+            }
+            else
+            {
+                mode = "patrol";
+            }
+        }
+
+        if (e.Name == "Aadi")
+        {
+            e.Position = enemyPosition;
+            e.Rotation = enemyRotation;
+            if (Ship.currentHP > 40)
+            {
+                mode = "hunt";
+            }
+            else
+            {
+                mode = "patrol";
+            }
+        }
+
+        if (e.Name == "Sjoeke")
+        {
+            e.Position = enemyPosition;
+            e.Rotation = enemyRotation;
+            if (Ship.currentHP > 40)
+            {
+                mode = "hunt";
+            }
+            else
+            {
+                mode = "patrol";
+            }
+        }
+
     }
-
-
-    
 
 }
